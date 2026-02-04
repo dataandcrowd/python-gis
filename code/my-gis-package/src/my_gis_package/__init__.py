3abd11b64629855a99eca02d3c289be22f92c770
Hyesop <shinhyesop@gmail.com>
2026-02-05 10:48:00 +1300

    Update __init__.py

--- code/my-gis-package/src/my_gis_package/__init__.py
+++ code/my-gis-package/src/my_gis_package/__init__.py
@@ -1,2 +1,6 @@
-def main() -> None:
-    print("Hello from my-gis-package!")
+from importlib import resources
+import pandas as pd
+
+def load_sample_census_nz() -> pd.DataFrame:
+    data_path = resources.files("my_gis_package").joinpath("data", "sample_census_nz.csv")
+    return pd.read_csv(data_path)
